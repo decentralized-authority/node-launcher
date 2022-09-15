31802003f093aa2b2ba21fbfd8c33e80593144fe
Ian Culp <icculp@gmail.com>
2022-09-15 16:17:20 +0000

    updated config for beacon rpc

--- src/types/ethereum/config/prysm.ts
+++ src/types/ethereum/config/prysm.ts
@@ -1,8 +1,9 @@
 export const beacon = `
 datadir: "/root/data"
 accept-terms-of-use: true
-beacon-rpc-provider: "0.0.0.0:{{RPC_PORT}}"
+rpc-http-enabled: true
 rpc-host: "0.0.0.0"
+rpc-port: {{RPC_PORT}}
 p2p-tcp-port: {{PEER_PORT}}
 p2p-udp-port: {{PEER_PORT}}
 execution-endpoint: "http://192.168.100.2:8550"
@@ -18,6 +19,6 @@ wallet-password-file: "/home/validator/.eth2validators/wallet-password.txt"
 #should be wallet dir flag
 # http-web3provider: "http://YYY.YYY.YYY.YYY:8545" deprecated
 graffiti: "YOUR_GRAFFITI_HERE"
-beacon-rpc-provider: "0.0.0.0:{{RPC_PORT}}"
+beacon-rpc-provider: "0.0.0.0:{{RPC_PORT}}" #not used for beacon
 --jwt-secret value  #REQUIRED if connecting to an execution node via HTTP. Provides a path to a file containing a hex-encoded string representing a 32 byte secret used for authentication with an execution node via HTTP. If this is not set, all requests to execution nodes via HTTP for consensus-related calls will fail, which will prevent your validators from performing their duties. This is not required if using an IPC connection.
 `;
