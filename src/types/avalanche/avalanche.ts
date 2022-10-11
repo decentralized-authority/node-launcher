2f6ae11a9950bb004fd0a1c6370fa79dd4c732b0
rBurgett <ryan@burgettdev.net>
2022-10-11 15:00:24 -0400

    Avalanchego 1.9.0

--- src/types/avalanche/avalanche.ts
+++ src/types/avalanche/avalanche.ts
@@ -37,6 +37,20 @@ export class Avalanche extends Bitcoin {
       case NodeClient.CORE:
         versions = [
           {
+            version: '1.9.0',
+            clientVersion: '1.9.0',
+            image: 'avaplatform/avalanchego:v1.9.0',
+            dataDir: '/root/db',
+            walletDir: '/root/keystore',
+            logDir: '/root/logs',
+            configDir: '/root/config',
+            networks: [NetworkType.MAINNET],
+            breaking: false,
+            generateRuntimeArgs(data: CryptoNodeData): string {
+              return ` --config-file=${path.join(this.configDir, Avalanche.configName(data))}`;
+            },
+          },
+          {
             version: '1.8.6',
             clientVersion: '1.8.6',
             image: 'avaplatform/avalanchego:v1.8.6',
