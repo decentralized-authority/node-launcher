f86cd9bd6e175ec25125c0376c4d9f404cb1d42b
Ian Culp <icculp@gmail.com>
2022-10-27 05:16:43 +0000

    filter contract DepositEvents for pubkey

--- src/util/crypto.ts
+++ src/util/crypto.ts
@@ -4,22 +4,23 @@ const algorithm = 'aes-256-cbc'; //Using AES encryption
 const iv = crypto.randomBytes(16);
 
 
+export function hexPrefix(pubkey: string) {
+    if (pubkey.startsWith('0x')) {
+    return pubkey;
+  } else {
+    return '0x' + pubkey;
+  }
+}
 
-//Encrypting text
 export function encrypt(text: string, password: string) {
-    //can hash password with sha256, will always create 32 byte hash
-    console.log(11);
+   //can hash password with sha256, will always create 32 byte hash
    const passwordHash = crypto.createHash('sha256').update(password, 'utf8').digest('base64').slice(0, 32)
-   console.log(13);
    const cipher = crypto.createCipheriv(algorithm, Buffer.from(passwordHash), iv);
-   console.log(15);
    const encrypted = cipher.update(text);
-   console.log(17);
    const encryptedData = Buffer.concat([iv, encrypted, cipher.final()]);
    return encryptedData.toString('base64');
 }
 
-// Decrypting text
 export function decrypt(encryptedData: string, password: string) {
   const passwordHash = crypto.createHash('sha256').update(password, 'utf8').digest('base64').slice(0, 32)
   const encryptedDataBuffer = Buffer.from(encryptedData, 'base64')
